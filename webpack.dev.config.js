bbe560ea9ca4c89dc920c0c928ea1bfb86dd757a
Max Rdb <rdbmax@gmail.com>
2017-08-23 22:52:36 +0200

    add webpack dashboard

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,6 +1,9 @@
+const DashboardPlugin = require('webpack-dashboard/plugin');
 const config = require('./webpack.config.js');
 
 config.devtool = 'inline-source-map';
+
+config.plugins.push(new DashboardPlugin());
 
 config.devServer = {
   contentBase: './dist',
