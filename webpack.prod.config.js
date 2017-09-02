64fa9dc317acdd6b0e9c00712a36d299282207b9
Max Rdb <rdbmax@gmail.com>
2017-09-02 14:59:37 +0200

    add optimization for production build

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,6 +1,14 @@
+const webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const config = require('./webpack.config.js');
 
+config.devtool = 'cheap-module-source-map';
+
 config.plugins.push(new UglifyJSPlugin());
+config.plugins.push(new webpack.DefinePlugin({
+  'process.env': {
+    NODE_ENV: JSON.stringify('production'),
+  },
+}));
 
 module.exports = config;
